172e9c5a0cfe83e00e392589c470655605b6325b
saquibansari99 <105703040+saquibansari98@users.noreply.github.com>
2023-11-18 15:32:40 +0530

    create header components

---
+++ src/components/Header.jsx
@@ -0,0 +1,50 @@
+// import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
+
+export default function Header() {
+  // const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  // const toggleMobileMenu = () => {
+  //   setMobileMenuOpen(!isMobileMenuOpen);
+  // };
+
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Sahand</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-transparent focus:outline-none"
+          />
+          <FaSearch className="text-slate-600 " />
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to="/sign-in">
+            <li className=" text-slate-700 hover:underline">Sign</li>
+          </Link>
+          {/* <Link to="/profile">
+            <li className=" text-slate-700 hover:underline">Sign</li>
+          </Link> */}
+        </ul>
+      </div>
+    </header>
+  );
+}
